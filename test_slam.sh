3e37c6dd2bd629a134ffb1643cd206629665ee37
HarisAshraf <harisashraf@aol.com>
2023-01-20 02:59:32 +0000

    Used my own world

--- test_slam.sh
+++ test_slam.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-
-xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch" &
+xterm -e " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find my_robot)/worlds/haris.world " &
 sleep 5
-xterm -e "roslaunch turtlebot_gazebo gmapping_demo.launch" &
+xterm -e " roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find my_robot)/maps/map.yaml " &
 sleep 5
-xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+xterm -e " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
 sleep 5
-xterm -e "roslaunch turtlebot_teleop keyboard_teleop.launch" &
+xterm -e " roslaunch turtlebot_teleop keyboard_teleop.launch" 
